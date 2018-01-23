923687da5f85bc8baa7629bd2d570da560bf7916
Lorenzo Lopez <lore64210@gmail.com>
2018-01-23 19:03:36 -0300

    arregle el index

--- scripts/acid.js
+++ scripts/acid.js
@@ -43,42 +43,42 @@ function mostrarImg(num){
 
 	switch(num){
 		case 1:
-			document.getElementById("img-principal").style.backgroundImage="url('../galeria/img1.jpg')";
+			document.getElementById("img-principal").style.backgroundImage="url('galeria/img1.jpg')";
 			document.getElementById("descripcion-img1").style.display="block";
 			document.getElementById("tit-descripcion1").style.display="block";
 			break;
 		case 2:
-			imagen.style.backgroundImage="url('../galeria/img2.jpg')";
+			imagen.style.backgroundImage="url('galeria/img2.jpg')";
 			document.getElementById("descripcion-img2").style.display="block";
 			document.getElementById("tit-descripcion2").style.display="block";
 			break;
 		case 3:
-			imagen.style.backgroundImage="url('../galeria/img3.jpg')";
+			imagen.style.backgroundImage="url('galeria/img3.jpg')";
 			document.getElementById("descripcion-img3").style.display="block";
 			document.getElementById("tit-descripcion3").style.display="block";
 			break;
 		case 4:
-			imagen.style.backgroundImage="url('../galeria/img4.jpg')";
+			imagen.style.backgroundImage="url('galeria/img4.jpg')";
 			document.getElementById("descripcion-img4").style.display="block";
 			document.getElementById("tit-descripcion4").style.display="block";
 			break;
 		case 5:
-			imagen.style.backgroundImage="url('../galeria/img5.jpg')";
+			imagen.style.backgroundImage="url('galeria/img5.jpg')";
 			document.getElementById("descripcion-img5").style.display="block";
 			document.getElementById("tit-descripcion5").style.display="block";
 			break;
 		case 6:
-			imagen.style.backgroundImage="url('../galeria/img6.jpg')";
+			imagen.style.backgroundImage="url('galeria/img6.jpg')";
 			document.getElementById("descripcion-img6").style.display="block";
 			document.getElementById("tit-descripcion6").style.display="block";
 			break;
 		case 7:
-			imagen.style.backgroundImage="url('../galeria/img7.jpg')";
+			imagen.style.backgroundImage="url('galeria/img7.jpg')";
 			document.getElementById("descripcion-img7").style.display="block";
 			document.getElementById("tit-descripcion7").style.display="block";
 			break;
 		case 8:
-			imagen.style.backgroundImage="url('../galeria/img8.jpg')";
+			imagen.style.backgroundImage="url('galeria/img8.jpg')";
 			document.getElementById("descripcion-img8").style.display="block";
 			document.getElementById("tit-descripcion8").style.display="block";
 			break;			
